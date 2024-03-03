b21cae3190b09828a871724772f44b57eff19155
Ethan TAYLAN <ethantaylan@MacBook-Pro-de-Ethan.local>
2024-03-04 00:48:46 +0100

    feat: snake keyboard events

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import Logo from "./assets/logo.png";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [rows, setRows] = useState<string[]>([]);
+  const [columns, setColumns] = useState<string[]>([]);
+
+  const [snakeX, setSnakeX] = useState<number>(10);
+  const [snakeY, setSnakeY] = useState<number>(10);
+
+  // const [snakePosition, setSnakePosition] = useState<number>()
+  const i = 15;
+
+  useEffect(() => {
+    if (rows.length < i) {
+      setRows(Array.from({ length: i }, () => ""));
+    }
+
+    if (columns.length < i) {
+      setColumns(Array.from({ length: i }, () => ""));
+    }
+  }, [rows, columns]);
+
+  useEffect(() => {
+    const pos = document.querySelector(`#pos-${snakeY}-${snakeX}`);
+
+    pos?.classList.add("bg-red-500");
+
+    console.log(snakeX);
+  }, [snakeX, snakeY]);
+
+  const keyPress = (e: KeyboardEvent) => {
+    switch (e.which) {
+      case 37: {
+        // left
+        setSnakeX((elementPosition) => elementPosition - 1);
+        break;
+      }
+      case 38: {
+        // up
+        setSnakeY((elementPosition) => elementPosition - 1);
+        break;
+      }
+      case 39: {
+        // right
+        setSnakeX((elementPosition) => elementPosition + 1);
+        break;
+      }
+      case 40: {
+        // down
+        setSnakeY((elementPosition) => elementPosition + 1);
+        break;
+      }
+      default:
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+    return () => {
+      document.removeEventListener("keydown", keyPress);
+    };
+  }, []);
+
   return (
     <div className="p-10 text-center flex flex-col justify-center items-center w-screen h-screen">
-      <img className="rotate-center" width={350} src={Logo} />
-      <h1 className="text-7xl font-bold text-white">Fastplate</h1>
-      <h2
-        style={{ color: "#ed244e" }}
-        className="tracking-wider text-sm font-semibold mt-5"
-      >
-        React / React Router / TypeScript / Tailwind / DaisyUI / Heroicons /
-        Axios / SweetAlert2
-      </h2>
-      <p className="mt-10 w-1/2 text-xl">
-        This template empowers you to swiftly build responsive and dynamic web
-        applications by harnessing the capabilities of the stacks named below.
-      </p>
-      <p className="text-2xl text-white mt-20">
-        Get started by editing{" "}
-        <span style={{ color: "#ed244e" }}>App.tsx</span> file
-      </p>
+      {Array.from({ length: i }, (_, rowIndex) => (
+        <div key={rowIndex} className="flex">
+          {Array.from({ length: i }, (_, colIndex) => (
+            <div
+              id={`pos-${rowIndex}-${colIndex}`}
+              key={colIndex}
+              style={{ padding: 15, margin: 1 }}
+              className="flex bg-gray-700"
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
